fa6c40c3adb63c1f6a1630d39e0b7bfe8df8233b
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-25 02:24:57 -0300

    update index.js

--- index.js
+++ index.js
@@ -2,30 +2,48 @@ async function CEP(cep) {
   try {
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     const data = await response.json();
-    console.log(data);
+    
+    if (response && data) {
+      return data;
+    };
+    
+    return false;
   } catch (error) {
     console.error("Erro ao buscar CEP:", error);
-  }
-}
+    return false;
+  };
+};
 async function CNPJ(cnpj) {
   try {
     const response = await fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj}`);
     const data = await response.json();
-    console.log(data);
+    
+    if (response && data) {
+      return data;
+    };
+    
+    return false;
   } catch (error) {
     console.error("Erro ao buscar CEP:", error);
-  }
-}
+    return false;
+  };
+};
 async function IP(ip) {
   try {
     const response = await fetch(`http://ipwho.is/${ip}`);
     const data = await response.json();
-    console.log(data);
+    
+    if (response && data) {
+      return data;
+    };
+    
+    return true;
   } catch (error) {
     console.error("Erro ao buscar CEP:", error);
-  }
-}
+    return false;
+  };
+};
 
-IP("")
-// CEP(96090608);
+IP("193.186.4.236").then(console.log);
+// CEP(96090600);
 // CNPJ(17005063000133);
